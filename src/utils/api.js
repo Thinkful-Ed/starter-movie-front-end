553f7aa59d212e5f13666717b1a5cab2e4247469
hchia444 <82906853+hchia444@users.noreply.github.com>
2022-11-18 16:37:00 -0500

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5001";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
